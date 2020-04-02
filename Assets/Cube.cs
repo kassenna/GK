4086b1e1b6783fbdf5114711cd7e833dbd41b383
katarzynamajgier3a2gim@gmail.com <katarzynamajgier3a2gim@gmail.com>
2020-04-02 14:10:15 +0200

    ssskk
    Signed-off-by: katarzynamajgier3a2gim@gmail.com <katarzynamajgier3a2gim@gmail.com>

---
+++ Assets/Cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+   public float jumpSpeed = 5f;//or whatever you want it to be
+   public int jump = 2;
+   public int j;
+   void Start ()
+   {
+      j = jump;
+   }
+ 
+ void Update(){
+    Rigidbody rb= GetComponent<Rigidbody>();
+       if (Input.GetKeyDown (KeyCode.Space ) && j>0 ){
+          rb.velocity += jumpSpeed * Vector3.up;
+          j--;
+       }
+    }
+ void OnCollisionEnter(Collision coll)
+ {
+    
+  
+       
+    {
+       if(coll.gameObject.tag == "JUMP")
+          j = jump;
+    }
+ }
+}
