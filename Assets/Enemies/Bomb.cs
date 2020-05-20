8fc09f4dc53b20da850b3b43761ff00f012a4aaa
katarzynamajgier3a2gim@gmail.com <katarzynamajgier3a2gim@gmail.com>
2020-05-20 21:28:38 +0200

    menu
    Signed-off-by: katarzynamajgier3a2gim@gmail.com <katarzynamajgier3a2gim@gmail.com>

--- Assets/Enemies/Bomb.cs
+++ Assets/Enemies/Bomb.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    private bool isTouched = false;
+    public float velocity = 1;
+    public float timeExplosion = 1;
+    public float rotate = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.tag = "Untagged";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTouched)
+        {
+            var rb = GetComponent<Rigidbody>();
+            rb.MovePosition(transform.position + (transform.forward) * (Time.deltaTime * velocity));
+            transform.Rotate(Vector3.up * rotate*Time.deltaTime, Space.Self);
+            timeExplosion -= Time.deltaTime;
+            if (timeExplosion < 0)
+            {
+                tag = "enemies";
+                var ani = GetComponent<Animator>();
+                ani.SetTrigger("explosion");
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            isTouched = true;
+            var ani = GetComponent<Animator>();
+            ani.SetBool("walk", true);
+        }
     }
 }
--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,27 +1,47 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-public class Menu : MonoBehaviour
-{
-    
+public class Menu : MonoBehaviour {
 
-    public Button button1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Canvas quitMenu;;
+	public Button btnLevel;
+	public Button btnExit;
+	/** Obiekt menu.*/
+	private Canvas manuUI;
+	
+	void Start (){
+	
+		Time.timeScale = 0;
+		Cursor.visible = manuUI.enabled;
+		Cursor.lockState = CursorLockMode.Confined;//Odblokowanie kursora myszy.
+	}
 
-    private void OnMouseDown()
-    {
-    }
+	
+	public void bExit() {
+		quitMenu.enabled = true; //Uaktywnienie meny z pytaniem o wyjście
+		btnLevel.enabled = false; //Deaktywacja przycsiku 'Start'.
+		btnExit.enabled = false; //Deaktywacja przycsiku 'Wyjście'.
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+	//Metoda wywoływana podczas udzielenia odpowiedzi przeczącej na pytanie o wyjście z gry.
+	public void bNo(){
+		quitMenu.enabled = false; 
+		btnLevel.enabled = true;
+		btnExit.enabled = true; 
+	}
+
+	//Metoda wywoływana przez przycisk uruchomienia gry 'Play Game'
+	public void bPlay (){
+		//Application.LoadLevel (0); //this will load our first level from our build settings. "1" is the second scene in our game	
+		manuUI.enabled = false; 
+		Time.timeScale = 1;
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		
+	}
+
+	public void bYes () {
+		Application.Quit(); 
+		
+	}
